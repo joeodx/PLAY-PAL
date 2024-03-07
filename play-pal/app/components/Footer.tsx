a0ecdf8456015a99377a7294e26c9b5b81e4fb63
jamesthecooke <jamesjcooke03@gmail.com>
2024-03-07 16:30:27 +0000

    added footer component and rendered it within page.tsx

---
+++ play-pal/app/components/Footer.tsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const Footer = () => {
+  return (
+    <footer className="order-double: solid border black p-5">
+      <h1 className="flex place-content-center text-xl ">this is a footer</h1>
+    </footer>
+  );
+};
+
+export default Footer;
--- play-pal/app/page.tsx
+++ play-pal/app/page.tsx
@@ -1,14 +1,17 @@
-
-import Header from "./components/Header"
+import Header from "./components/Header";
+import Footer from "./components/Footer";
 
 export default function Home() {
   return (
-  <>
-  <Header />
-  <p className="sm:pt-20">Tired of playing with yourself, now find others to play with.</p>
-  <h2 className="text-3xl">Introducing..... PLAY-PAL, play with each other all night long</h2>
-  
-  </>  
-  
+    <>
+      <Header />
+      <p className="sm:pt-20">
+        Tired of playing with yourself, now find others to play with.
+      </p>
+      <h2 className="text-3xl">
+        Introducing..... PLAY-PAL, play with each other all night long
+      </h2>
+      <Footer />
+    </>
   );
 }
