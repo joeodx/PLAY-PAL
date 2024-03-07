f68a740abfcdbe4b67231547dbc3465b8320226d
Joe O'Donovan <jojoodonovan@hotmail.com>
2024-03-07 16:21:25 +0000

    addedHeader

--- play-pal/app/components/Header.tsx
+++ play-pal/app/components/Header.tsx
@@ -1,11 +1,20 @@
 import React from 'react'
+import Link from 'next/link'
 
 const Header = () => {
   return (
     <>
-    <div>
-        <header><div></div></header>
-    </div>
+    
+    <header className="order-double: solid border black p-5">
+      <div className="flex place-content-center ">
+       <Link href="/">
+        Play-Pal 
+       </Link>
+       </div>
+      <div>
+       <p className="flex place-content-center">Tired of playing with yourself, now find others to play with.</p>
+       </div> {/* </header>Play-Pal</h1> */}
+    </header>
     
     </>
   )
