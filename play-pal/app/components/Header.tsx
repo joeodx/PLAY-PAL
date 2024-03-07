1bff81123c44ddc5007f60a73d3b42ae6e90a287
James Robertson <jaarobertson92@gmail.com>
2024-03-07 15:48:37 +0000

    initial file set up. Starting header component

--- play-pal/app/components/Header.tsx
+++ play-pal/app/components/Header.tsx
@@ -3,7 +3,10 @@ import React from 'react'
 const Header = () => {
   return (
     <>
-        <h1>Welcome To PlayPal</h1>
+    <div>
+        <header><div></div></header>
+    </div>
+    
     </>
   )
 }
---
+++ play-pal/app/page.tsx
@@ -0,0 +1,13 @@
+import Image from "next/image";
+import HomePage from "./Home/page";
+
+export default function Home() {
+  return (
+  <>
+  <p className="sm:pt-20">Tired of playing with yourself, now find others to play with.</p>
+  <h2 className="text-3xl">Introducing..... PLAY-PAL, play with each other all night long</h2>
+  <HomePage></HomePage>
+  </>  
+  
+  );
+}
