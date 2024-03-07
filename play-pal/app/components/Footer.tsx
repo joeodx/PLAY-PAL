8269b121fb0cb038d48efac46da937f6a3069b7f
jamesthecooke <jamesjcooke03@gmail.com>
2024-03-07 16:31:56 +0000

    moved footer to bottom of the page

--- play-pal/app/components/Footer.tsx
+++ play-pal/app/components/Footer.tsx
@@ -2,7 +2,7 @@ import React from "react";
 
 const Footer = () => {
   return (
-    <footer className="order-double: solid border black p-5">
+    <footer className="fixed inset-x-0 bottom-0 order-double: solid border black p-5">
       <h1 className="flex place-content-center text-xl ">this is a footer</h1>
     </footer>
   );
