0d4158402bfe725a603a019275cdd11d12b8b0d6
Joe O'Donovan <jojoodonovan@hotmail.com>
2024-03-08 20:14:00 +0000

    added

--- play-pal/app/components/Footer.tsx
+++ play-pal/app/components/Footer.tsx
@@ -3,7 +3,7 @@ import React from "react";
 const Footer = () => {
   return (
     <footer className="fixed inset-x-0 bottom-0 order-double: solid border black p-5">
-      <h1 className="flex place-content-center text-xl ">
+      <h1 className="flex place-content-center text-l ">
       © Play-Pal 
        All Rights Reserved
       </h1>
