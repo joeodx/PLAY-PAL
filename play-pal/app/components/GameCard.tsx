76608a3de0abc18fa8537f772fa9933bec321548
jamesthecooke <jamesjcooke03@gmail.com>
2024-03-12 12:32:07 +0000

    added data to the game cards

--- play-pal/app/components/GameCard.tsx
+++ play-pal/app/components/GameCard.tsx
@@ -1,24 +1,36 @@
-import React from 'react'
+import React from "react";
 import {
-    Card,
-    CardContent,
-    CardDescription,
-    CardTitle,
-  } from "@/components/ui/card"
-  import Image from 'next/image'
+  Card,
+  CardContent,
+  CardDescription,
+  CardTitle,
+} from "@/components/ui/card";
+import Image from "next/image";
 
-function GameCard() {
-  return (
-    <Card>
-    <CardTitle></CardTitle>
-    <CardDescription>Card Description</CardDescription>
-  <CardContent>
-    <p>Card Content</p>
-  </CardContent>
-  <Image />
-</Card>
-
-  )
+interface img {
+  id: number;
+  title: string;
+  url: string;
 }
 
-export default GameCard
+async function GameCard() {
+  const res = await fetch("https://jsonplaceholder.typicode.com/photos");
+  const data: img[] = await res.json();
+
+  return (
+    <>
+      {data.map((img) => (
+        <Card key={img.id}>
+          <CardTitle>{img.title}</CardTitle>
+          <CardDescription>Card Description</CardDescription>
+          <CardContent>
+            <p>Card Content</p>
+          </CardContent>
+          {/* <Image /> */}
+        </Card>
+      ))}
+    </>
+  );
+}
+
+export default GameCard;
--- play-pal/app/page.tsx
+++ play-pal/app/page.tsx
@@ -1,14 +1,14 @@
 import Header from "./components/Header";
 import Footer from "./components/Footer";
+import GameCard from "./components/GameCard";
 
 export default function Home() {
   return (
     <>
       <Header />
 
-      <h2 className="text-3xl text-center pt-10">
-        Pick a game below! 
-      </h2>
+      <h2 className="text-3xl text-center pt-10">Pick a game below!</h2>
+      <GameCard />
       <Footer />
     </>
   );
