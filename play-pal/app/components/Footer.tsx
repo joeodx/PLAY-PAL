9896b5d97f4a94b1a775533fc7355c1ebc82961b
Joe O'Donovan <jojoodonovan@hotmail.com>
2024-03-15 18:23:24 +0000

    addedFooterCss

--- play-pal/app/components/Footer.tsx
+++ play-pal/app/components/Footer.tsx
@@ -2,7 +2,7 @@ import React from "react";
 
 const Footer = () => {
   return (
-    <footer className="fixed inset-x-0 bottom-0 border-double: solid border black p-5 bg-gradient-to-t from-gray-200/10">
+    <footer className="fixed inset-x-0 bottom-0 border-double: solid border black transition-opacity duration-300 cursor-pointer bg-slate-900" >
       <h1 className="flex place-content-center text-l ">
       © Play-Pal 
        All Rights Reserved
