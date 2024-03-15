cef3a1dd21204cddb30ee9881c1d1e9541c2df9b
Joe O'Donovan <jojoodonovan@hotmail.com>
2024-03-15 18:09:13 +0000

    added shadow

--- play-pal/app/components/GameCard.tsx
+++ play-pal/app/components/GameCard.tsx
@@ -31,7 +31,7 @@ const GameCard = () => {
     <>
     <div className="w-[50vw] align-items-center">
       {games.map((game) => (
-        <Card className="my-10" key={game.id}> {/* Use the game's id as a key for React's list rendering */}
+        <Card className="my-10 shadow-inner shadow-slate-900 " key={game.id}> {/* Use the game's id as a key for React's list rendering */}
           <CardTitle className="pl-14 pt-4">{game.name}</CardTitle>
           {/* <CardDescription className="pl-14">{game.background_image}</CardDescription> */}
           <CardContent className="pl-14">
