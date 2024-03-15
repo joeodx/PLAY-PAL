ae00f0a4161b6b9f78036b2eca60e51ca2e9180a
Joe O'Donovan <jojoodonovan@hotmail.com>
2024-03-15 18:17:31 +0000

    addedPaddingBottom

--- play-pal/app/components/GameCard.tsx
+++ play-pal/app/components/GameCard.tsx
@@ -29,7 +29,7 @@ const GameCard = () => {
 
   return (
     <>
-    <div className="w-[50vw] align-items-center">
+    <div className="w-[50vw] align-items-center pb-7">
       {games.map((game) => (
         <Card className="my-10 shadow-inner shadow-slate-900 " key={game.id}> {/* Use the game's id as a key for React's list rendering */}
           <CardTitle className="pl-14 pt-4">{game.name}</CardTitle>
